8d90ea06506971ce5dd1328719d443d6f06360ee
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-08-29 14:20:08 -0300

    melhorando css

--- projeto-games/src/App.jsx
+++ projeto-games/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
       id: 1,
       nome: "Tokyo Revengers",
       imagemUrl:
-        "https://animesonline.cc/wp-content/uploads/2021/04/e77ZdXs1tFmpTOiFfEZSlcz9ZWN.jpg",
+        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/e77ZdXs1tFmpTOiFfEZSlcz9ZWN.jpg",
       genero: "Ficção científica",
       link: "https://animesonline.org/animes/tokyo-revengers/",
     },
@@ -18,7 +18,7 @@ export default function App() {
       id: 2,
       nome: "tensei shitara slime datta ken",
       imagemUrl:
-        "https://blogs.opovo.com.br/bancadoanime/wp-content/uploads/sites/59/2019/03/game-740x413.jpg",
+        "https://cdn.fstatic.com/media/movies/covers/2019/12/tWJRkoAMGTLLT8q8IIahJWpvb1n_Ov6ZjP6.jpg",
       genero: "Isekai",
       link: "https://animesonline.org/animes/tensei-shitara-slime-datta-ken/",
     },
@@ -26,7 +26,7 @@ export default function App() {
       id: 3,
       nome: "kaifuku jutsushi no yarinaoshi",
       imagemUrl:
-        "https://media.fstatic.com/OWKUMNqs2wNbOKSr_2JGbgJ7u6c=/290x478/smart/media/movies/covers/2020/03/Kaifuku-Jutsushi-no-Yarinaoshi-vai-ser-anime-1.jpg",
+        "https://animesonehd.xyz/wp-content/uploads/2020/12/Kaifuku-Jutsushi-no-Yarinaoshi-online-em-HD.jpg",
       genero: "Fantasia sombria",
       link: "https://animesonline.org/animes/kaifuku-jutsushi-no-yarinaoshi/",
     },
@@ -59,10 +59,18 @@ export default function App() {
     setAnimes(animes.filter((anime, indiceAnime) => indiceAnime !== indice));
   };
 
+
+
   const verificaExisteAnime = () =>
     animes.some(
       (anime) => anime.nome === nomeAnime && anime.imagemUrl === imagemUrlAnime
     );
+
+  const verificaInputVazio = () =>{
+    if(!nomeAnime || !imagemUrlAnime){
+      return true
+    }
+  }
 
   const editaAnime = () => {
     if (verificaExisteAnime()) {
@@ -76,12 +84,11 @@ export default function App() {
         return anime;
       });
 
-
-
       setAnimes(animesAtualizados);
       setEditando(false);
       setIndiceEditando(null);
       limpaInput();
+      ativaFormulario(false)
     }
   };
 
@@ -91,6 +98,10 @@ export default function App() {
   };
 
   const Adiciona = () => {
+
+   if (verificaInputVazio()) {
+     alert("Insira as informações corretamente")
+   } else {
     setAnimes([
       ...animes,
       {
@@ -99,6 +110,10 @@ export default function App() {
       },
     ]);
     limpaInput();
+
+   }
+
+   
   };
 
   const adicionaAnime = (evento) => {
@@ -107,14 +122,56 @@ export default function App() {
     editando ? editaAnime() : Adiciona();
   };
 
+  const [formularioAtivo, ativaFormulario] = useState(false)
+
+  
+
   return (
     <>
       <Cabecalho titulo="AnimeBox" />
 
-      <div className="container">
-        <h1>Adicione um anime</h1>
+      <div className="Container">
+        {animes.map((anime, indice) => (
+          <>
+            <div className="Box" keys={anime.id}>
+              <h3>{anime.nome}</h3>
+              <img src={anime.imagemUrl} alt={anime.nome} />
 
-        <form onSubmit={adicionaAnime}>
+              <p className="genero">{anime.genero}</p>
+              <div className="botoes">
+                <a href={anime.link}>
+                  <button>Assistir</button>
+                </a>
+                <button type="button" onClick={() => deletarAnime(indice)}>
+                  Excluir
+                </button>
+
+                <button
+                  type="button"
+                  onClick={() => {
+                    setEditando(true);
+                    setIndiceEditando(indice);
+                    ativaFormulario(true)
+                  }}
+                >
+                  Editar
+                </button>
+              </div>
+            </div>
+
+          </>
+        ))}
+      </div>
+      
+      <div className="Container " >
+       
+        <button className="adicionaAnime" onClick={ () => ativaFormulario(true)}>Adicione um novo anime</button>
+      
+      {
+        formularioAtivo ?  
+        <div >
+
+        <form onSubmit={adicionaAnime} className="formulario">
           <input
             type="text"
             placeholder="digite o nome do anime"
@@ -137,35 +194,14 @@ export default function App() {
           <br />
           <button type="submit">Salvar</button>
         </form>
-      </div>
+      </div> : console.log("clique em editar para abrir o formulário de cadastro de anime")
 
-      <div className="Container">
-        {animes.map((anime, indice) => (
-          <>
-            <div className="Box" keys={anime.id}>
-              <h3>{anime.nome}</h3>
-              <img src={anime.imagemUrl} alt={anime.nome} />
-              <p>{anime.genero}</p>
-              <a href={anime.link}>
-                <button>Assistir</button>
-              </a>
-              <button type="button" onClick={() => deletarAnime(indice)}>
-                Excluir
-              </button>
+      }
+           
+    
+       </div>
 
-              <button
-                type="button"
-                onClick={() => {
-                  setEditando(true);
-                  setIndiceEditando(indice);
-                }}
-              >
-                Editar
-              </button>
-            </div>
-          </>
-        ))}
-      </div>
+     
     </>
   );
 }
--- projeto-games/src/components/layout/header/Cabecalho.jsx
+++ projeto-games/src/components/layout/header/Cabecalho.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import './Cabecalho.css';
-import logo from "./logo-blue.gif"
+import logo from "./logoBlue.jpg"
+
 
 
 export default (props) =>  
     <header className="header">
-      
-      <img src={logo} className="logo"></img>
-
       <h1>{props.titulo}</h1>
 
-      <ul className="opcoes">
-        <li>Home</li>
-        <li>Inserir</li>
-      </ul>
 
     </header>
 
